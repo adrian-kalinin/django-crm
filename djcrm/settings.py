61382306ace6a0fbecf1afb9d7d2f6fa4cdea12d
Adrian Kalinin <adrian.kalinin@protonmail.com>
2021-07-10 01:17:40 +0300

    fix email backend for production

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -174,6 +174,7 @@ if not DEBUG:
 
     ALLOWED_HOSTS = ['*']
 
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = env('EMAIL_HOST')
     EMAIL_HOST_USER = env('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
